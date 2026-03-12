108996011e55b905c1eda4f35bbf38145d1136ef
Marcos Rosario <mhrosariom@gmail.com>
2026-03-12 19:15:30 -0400

    #01-javascript

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/mhrosariom.js
@@ -0,0 +1,25 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// COMENTARIO EN UNA LINEA
+
+/*
+COMENTARIO
+EN
+VARIAS
+LINEAS
+*/
+
+let myVariable1 = 'Soy una variable en JavaScript';
+const myConstant1 = 'Soy una constante en JavaScript';
+
+let string = 'String';
+let number = 4;
+let boolean = false;
+// let isUndefined = undefined;
+let isNull = null;
+let bigInt = 323434245435345345342342346756n;
+let simbol = Symbol('Unico');
+
+let language = 'JavaScript';
+
+console.log(`!Hola, ${language}¡`);
