5be6d144af3242f7182da97591b4d290f53ac2ad
Alexeis Veliz <alexeisveliz95@gmail.com>
2026-04-09 04:37:40 -0400

    00-Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/alexeisveliz95.py
@@ -0,0 +1,19 @@
+# URL del sitio oficial de Python https://www.python.org/
+
+# Esto es un comentario en una linea
+
+""""Esto es un comentario
+en varias lineas de codigo mientras 
+no se asigne a una variable"""
+
+mi_nombre = "Alexeis" #Variable que almacena un string
+COLOR_PELO = "Rubio" #Constante que almacena un string
+
+# Datos primitivos
+mi_string_var = "Cadeba de texto"   #Variable que almacena un string
+mi_numero_entero = 50               #Variable que almacena un numero entero
+mi_numero_flotante = 12.8           #Variable que almacena un numero flotante
+estudio_programacion = True         #Variable que almacena un valor booleano
+
+#print() es una funcion build-in que imprime por consola el parametro que se le pase
+print("Hola, Python")
